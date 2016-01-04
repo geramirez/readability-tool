b476d5c719d37a1ccd848a3fd4f3b6cb159e02ae
ramirezg <gabriel.ramirez@gsa.gov>
2016-01-03 21:50:54 -0500

    Adding more documentation and creating getStats function

--- sr-tool.go
+++ sr-tool.go
@@ -7,17 +7,25 @@ import (
 	"unicode/utf8"
 )
 
+//PUNCTUATION is a regex string for finding ".", "?", and "!".
 var PUNCTUATION = regexp.MustCompile("[.?!]+\\s")
+
+// WORD is a regex string for words
 var WORD = regexp.MustCompile("\\w+")
 
 func countSentences(text string) int {
 	return len(PUNCTUATION.Split(text, -1))
 }
 
+// getWords
 func getWords(text string) []string {
 	return WORD.FindAllString(text, -1)
 }
 
+// getLetterType returns the type of letter
+// 1 for weak vowel
+// 2 for strong vowel
+// 3 for consonant
 func getLetterType(letter rune) int {
 	switch letter {
 	case 'i', 'u':
@@ -28,6 +36,9 @@ func getLetterType(letter rune) int {
 	return 3
 }
 
+// countSyllables calculates the number of syllables using a modified
+// version of the syllabification algorithm for spanish found in
+// Heriberto Cuayáhuitl's article: http://www.dfki.de/~hecu01/publications/hc-cicling2004.pdf
 func countSyllables(word string) int {
 	word = strings.ToLower(word)
 	var letterType, consonantRun, vowelRun, syllables int
@@ -66,6 +77,7 @@ func countSyllables(word string) int {
 	return syllables
 }
 
+// getCounts calculates syllables, words, and sentences
 func getCounts(text string) (int, int, int) {
 	var totalSyllables, totalWords int
 	for _, word := range getWords(text) {
@@ -75,8 +87,17 @@ func getCounts(text string) (int, int, int) {
 	return totalSyllables, totalWords, countSentences(text)
 }
 
+// getStats calculates Fernandez Huerta's readability scores using an updated formula found here:
+// http://linguistlist.org/issues/22/22-2332.html
+// This function also returns syllables, words, and sentences.
+func getStats(text string) (int, int, int, float32) {
+	syllables, words, sentences := getCounts(text)
+	readability := 206.84 - 60.0*(float32(syllables)/float32(words)) - 102.0*(float32(sentences)/float32(words))
+	return syllables, words, sentences, readability
+}
+
 func main() {
-	syllables, words, sentences := getCounts("how are you doing? and why are you doing it. how do you know?")
-	fmt.Println(syllables, words, sentences)
+	syllables, words, sentences, readability := getStats("Hello")
+	fmt.Println(syllables, words, sentences, readability)
 
 }
