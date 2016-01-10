6cdcced5d81d500c515e47cf9cc81e8ffabca0f3
Gabriel Ramirez <gabriel.e.ramirez@gmail.com>
2016-01-09 23:41:53 -0500

    Sep. api and creating a document to guid users

---
+++ api.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"encoding/json"
+	"io"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/geramirez/readability-tool/scorer"
+)
+
+// serveStats collects stats from post request and returns stats in json format
+func serveStats(rw http.ResponseWriter, req *http.Request) {
+	body, _ := ioutil.ReadAll(req.Body)
+	stats := scorer.GetStats(string(body))
+	statsJSON, _ := json.Marshal(stats)
+	io.WriteString(rw, string(statsJSON))
+}
+
+func main() {
+	http.Handle("/", http.FileServer(http.Dir("./static")))
+	http.HandleFunc("/api", serveStats)
+	http.ListenAndServe(":8000", nil)
+}
